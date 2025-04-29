b8c82bb92c1af0721c19f30316410071216da21b
Thomas Doolittle <tdoolittle3@gmail.com>
2025-04-29 15:23:42 -0600

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Analytics/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
