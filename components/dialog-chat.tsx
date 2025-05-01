396296ef4e87b21d02ec1df21413d29a2f6ed0a7
Thomas Doolittle <tdoolittle3@gmail.com>
2025-05-01 12:52:14 -0600

    increase dialog time

--- components/dialog-chat.tsx
+++ components/dialog-chat.tsx
@@ -49,7 +49,7 @@ export default function DialogChat({ userInput, onDismiss }) {
 
         const timer = setTimeout(() => {
           onDismiss();
-        }, 5000);
+        }, 10000);
 
         return () => clearTimeout(timer);
       }
